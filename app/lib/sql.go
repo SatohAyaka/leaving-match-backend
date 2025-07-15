b9a5be26f356489986945259e42c8a5d92f52c16
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-07-15 18:33:41 +0900

    add: DBとの接続部分

---
+++ app/lib/sql.go
@@ -0,0 +1,39 @@
+package lib
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	once sync.Once
+	DB   *gorm.DB
+)
+
+func InitDB() *gorm.DB {
+	once.Do(func() {
+		dsn := fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+			os.Getenv("MYSQL_USER"),
+			os.Getenv("MYSQL_PASSWORD"),
+			os.Getenv("MYSQL_HOST"), // 例: localhost:3306
+			os.Getenv("MYSQL_PORT"),
+			os.Getenv("MYSQL_DATABASE"),
+		)
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+		if err != nil {
+			log.Fatalf("DB connect error: %v", err)
+		}
+		db.AutoMigrate(&model.BusTime{}, &model.Vote{}, &model.Result{})
+		DB = db
+	})
+	return DB
+}
