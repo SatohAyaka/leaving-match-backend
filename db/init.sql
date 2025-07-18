c59c49e476334516a5645eaca60f728c2c4afa86
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-07-18 12:44:07 +0900

    add: 初期DBの構造追加

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE BusTime_Data (
     bustime_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id INT, --','区切り
-    previous INT,
-    nearest INT,
-    next INT,
+    member_id VARCHAR(100), --','区切り
+    previous DATETIME,
+    nearest DATETIME,
+    next DATETIME,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 );
 
@@ -21,7 +21,8 @@ CREATE TABLE Vote_Data (
 CREATE TABLE Result_Data (
     result_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     bustime_id INT,
-    bus_time INT,
+    bus_time DATETIME,
+    member INT,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (bustime_id) REFERENCES BusTime_Data(bustime_id)
 );
