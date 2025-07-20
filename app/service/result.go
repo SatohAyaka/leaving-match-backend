4b3ce05bc94c80d0ab3209a5ad1305ab94187e4e
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-07-20 23:04:11 +0900

    add: Result_DataのGET機能追加

--- app/service/result.go
+++ app/service/result.go
@@ -1,6 +1,25 @@
 package service
 
+import (
+	"SatohAyaka/leaving-match-backend/lib"
+	"SatohAyaka/leaving-match-backend/model"
+	"log"
+)
+
 type ResultService struct{}
 
-func (ResultService) GetResult(busTimeId int64) {
+func (ResultService) GetResult(busTimeId int64) ([]model.Result, error) {
+	results := []model.Result{}
+
+	query := lib.DB.Model(&model.Result{})
+	if busTimeId > 0 {
+		query = query.Where("bustime_id = ?", busTimeId)
+	}
+
+	if err := query.Find(&results).Error; err != nil {
+		log.Printf("DBクエリエラー: %v", err)
+		return nil, err
+	}
+
+	return results, nil
 }
