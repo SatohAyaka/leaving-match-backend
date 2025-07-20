afefa292ccee5fd2a2e59933484181ec66c87f5f
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-07-20 23:03:32 +0900

    fix: 環境設定完了

--- app/lib/sql.go
+++ app/lib/sql.go
@@ -24,7 +24,7 @@ func InitDB() *gorm.DB {
 			"%s:%s@tcp(%s:%s)/%s?parseTime=true",
 			os.Getenv("MYSQL_USER"),
 			os.Getenv("MYSQL_PASSWORD"),
-			os.Getenv("MYSQL_HOST"), // 例: localhost:3306
+			os.Getenv("MYSQL_HOST"),
 			os.Getenv("MYSQL_PORT"),
 			os.Getenv("MYSQL_DATABASE"),
 		)
