832e68296bb96beb9e2ae70005998fce2ddba56a
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-07-21 01:38:09 +0900

    add: bustimeDBの作成とフロントでの時刻情報をtimeに変形する関数の追加

--- app/service/bustime.go
+++ app/service/bustime.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"SatohAyaka/leaving-match-backend/lib"
+	"SatohAyaka/leaving-match-backend/model"
+	"time"
+)
+
+type BusTimeService struct{}
+
+func (BusTimeService) CreateBusTime(member string, previous time.Time, nearest time.Time, next time.Time) (int64, error) {
+	bustime := model.BusTime{
+		MemberId:     member,
+		PreviousTime: previous,
+		NearestTime:  nearest,
+		NextTime:     next,
+	}
+
+	if err := lib.DB.Create(&bustime).Error; err != nil {
+		return 0, err
+	}
+
+	return bustime.BusTimeId, nil
+}
