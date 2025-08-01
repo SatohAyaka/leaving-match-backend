6059ed57cccf3a2fe2503073f0a8f3c5ec84b9ec
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-08-01 15:03:59 +0900

    fix: PredictionのmodelにBusTimeId追加

--- app/model/prediction.go
+++ app/model/prediction.go
@@ -4,6 +4,7 @@ import "time"
 
 type Prediction struct {
 	PredictionId   int64     `gorm:"column:prediction_id;primaryKey;autoIncrement"`
+	BusTimeId      int64     `gorm:"column:bustime_id"`
 	UserId         int64     `gorm:"column:user_id"`
 	PredictionTime time.Time `gorm:"column:prediction_time"`
 	CreatedDate    time.Time `gorm:"column:created_date;type:datetime;autoCreateTime"`
