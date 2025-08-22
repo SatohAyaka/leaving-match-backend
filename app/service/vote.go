b1e9a3dd61b4d3f9e34b10e9e9267033ec604f32
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-08-22 10:29:40 +0900

    fix: VoteのキーをBusTimeに戻した

--- app/service/vote.go
+++ app/service/vote.go
@@ -8,9 +8,9 @@ import (
 
 type VoteService struct{}
 
-func (VoteService) CreateVote(sammaryId int64, userId int64, previous bool, nearest bool, next bool) error {
+func (VoteService) CreateVote(bustimeId int64, userId int64, previous bool, nearest bool, next bool) error {
 	vote := model.Vote{
-		SammaryId: sammaryId,
+		BusTimeId: bustimeId,
 		UserId:    userId,
 		Previous:  previous,
 		Nearest:   nearest,
