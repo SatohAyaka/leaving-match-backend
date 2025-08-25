34fce1c3d7ab930346c7dcb24fb84bda3e7dfa41
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-08-25 20:02:10 +0900

    fix: model recommendedの修正

--- app/model/recommmended.go
+++ app/model/recommmended.go
@@ -1,10 +1,31 @@
 package model
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type IntSlice []int
+
+func (s IntSlice) Value() (driver.Value, error) {
+	return json.Marshal(s)
+}
+
+func (s *IntSlice) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to scan IntSlice: %v", value)
+	}
+	return json.Unmarshal(bytes, s)
+}
 
 type Recommended struct {
 	RecommendedId   int64     `gorm:"column:recommended_id;primaryKey;autoIncrement"`
 	RecommendedTime time.Time `gorm:"column:recommended_time"`
+	MemberIds       IntSlice  `gorm:"column:member_ids;type:json;not null"`
+	Status          bool      `gorm:"column:status"`
 	CreatedDate     time.Time `gorm:"column:created_date;type:datetime;autoCreateTime"`
 }
 
