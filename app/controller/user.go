35029ff6dc665f88aa04b2d2dff3ca201bfb6892
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-08-26 06:52:55 +0900

    add: getUser

--- app/controller/user.go
+++ app/controller/user.go
@@ -48,4 +48,43 @@ func CreateUserHandler(c *gin.Context) {
 
 func UpdateUserHandler(c *gin.Context) {}
 
-func GetUserHandler(c *gin.Context) {}
+func GetUserHandler(c *gin.Context) {
+	var backendUserId, staywatchUserId, slackUserId int64
+	var err error
+
+	backendUserQuery := c.Query("backend")
+	if backendUserQuery != "" {
+		staywatchUserId, err = strconv.ParseInt(backendUserQuery, 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	staywatchUserQuery := c.Query("staywatch")
+	if staywatchUserQuery != "" {
+		staywatchUserId, err = strconv.ParseInt(staywatchUserQuery, 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	slackUserQuery := c.Query("slack")
+	if slackUserQuery != "" {
+		slackUserId, err = strconv.ParseInt(slackUserQuery, 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	userName := c.Query("name")
+	userService := service.UserService{}
+	userData, err := userService.GetUser(backendUserId, staywatchUserId, slackUserId, userName)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to get user data"})
+		return
+	}
+	c.JSON(http.StatusOK, userData)
+}
--- app/service/user.go
+++ app/service/user.go
@@ -26,3 +26,28 @@ func (UserService) CreateUser(staywatchUserId int64, slackUserId int64, userName
 	}
 	return user.BackendUserId, nil
 }
+
+func (UserService) GetUser(backendUserId int64, staywatchUserId int64, slackUserId int64, userName string) ([]model.User, error) {
+	var users []model.User
+	db := lib.DB
+
+	// クエリパラメータがある場合のみ絞り込み
+	if backendUserId != 0 {
+		db = db.Where("backend_user_id = ?", backendUserId)
+	}
+	if staywatchUserId != 0 {
+		db = db.Where("staywatch_user_id = ?", staywatchUserId)
+	}
+	if slackUserId != 0 {
+		db = db.Where("slack_user_id = ?", slackUserId)
+	}
+	if userName != "" {
+		db = db.Where("user_name = ?", userName)
+	}
+
+	if err := db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
