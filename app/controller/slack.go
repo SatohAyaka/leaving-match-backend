0a8c376a8d58626d97e1f361418f72af6870c421
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-09-22 11:09:47 +0900

    fix: バスの時刻も送信する形に

--- app/controller/slack.go
+++ app/controller/slack.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"SatohAyaka/leaving-match-backend/lib"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -11,8 +12,19 @@ import (
 
 func SendDMHandler(c *gin.Context) {
 	membersQuery := c.QueryArray("member")
-	channels := make([]string, 0, len(membersQuery))
+	busTimesQuery := c.QueryArray("bustime")
 
+	busMessage := "乗れそうなバスを選択してください\n"
+	for i, minbustime := range busTimesQuery {
+		bustime, err := ParseQueryToTime(minbustime, "DM bustime")
+		if err != nil {
+			log.Println("failed to parse bustime:", minbustime, "err:", err)
+			continue
+		}
+		busMessage += fmt.Sprintf("%d. %s\n", i+1, bustime)
+	}
+
+	channels := make([]string, 0, len(membersQuery))
 	for _, m := range membersQuery {
 		staywatchId, err := strconv.ParseInt(m, 10, 64)
 		if err != nil {
@@ -36,5 +48,6 @@ func SendDMHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status":   "ok",
 		"channels": channels,
+		"message":  busMessage,
 	})
 }
